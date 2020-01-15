1d238a83eadd0310abeedf826b220aa063f3f7d3
robertzhao2002 <rzhao1234@gmail.com>
2020-01-15 18:12:13 -0500

    added timer, added intro, added algorithm to recognize user input on the screen

--- People.py
+++ People.py
@@ -37,9 +37,38 @@ def numColor(people, color):
         if p.color==color:
             f+=1
     return f
+def writeAt(t, message, x, y, color, size, align):
+    t.hideturtle()
+    t.penup()
+    t.goto(x, y)
+    t.color(color)
+    style = ('Courier', size, 'bold')
+    t.write(str(message), font=style, align=align)
+
+def writeAtB(t, message, x, y, color, size):
+    t.hideturtle()
+    t.penup()
+    t.goto(x, y)
+    t.color(color)
+    style = ('Courier', size, 'bold')
+    t.write(str(message), font=style)
 
 colors = ['red', 'orange', 'yellow', 'dark green', 'blue', 'purple', 'black']
 s = Screen()
+s.setup(1500, 800)
+s.bgcolor('black')
+rules = Turtle()
+writeAt(rules, 'People in a City', 0, 0, 'white', 30, 'center')
+time.sleep(5)
+rules.clear()
+writeAtB(rules, 'You have 10 seconds to observe the crowd.', -480, 150, 'white', 16)
+writeAtB(rules, 'Your score is the absolute value of the different between your guess and the actual amount.', -480, 120, 'white', 16)
+writeAtB(rules, 'Smaller score = better.', -480, 90, 'white', 16)
+writeAtB(rules, 'You have 2 seconds to type your answer on the screen.', -480, 60, 'white', 16)
+writeAtB(rules, 'Press enter to submit. Press delete to clear your response.', -480, 30, 'white', 16)
+writeAtB(rules, 'Good luck! Have fun.', -480, 0, 'white', 20)
+time.sleep(10)
+rules.clear()
 screencolor = colors[randint(0, len(colors)-1)]
 s.bgcolor(screencolor)
 people = []
@@ -126,65 +155,101 @@ def displayQuestion(t, color):
     t.write(displaystr, font=style, align='center')
 print(numcolorpeople)
 questionWriter = Turtle()
-questionWriter.penup()
 questionWriter.hideturtle()
+questionWriter.penup()
 displayQuestion(questionWriter, screencolor)
+digitsEntered = []
 t1 = Turtle()
 t1.hideturtle()
 t1.penup()
 t1.goto(-100, -50)
+timer = Turtle()
+timer.hideturtle()
+timer.penup()
+def countdown(t, seconds, time):
+    t.goto(-300,280)
+    for i in range(seconds, -1, -1):
+        t.color('black')
+        style = ('Courier', 20, 'bold')
+        t.write(str(i), font=style, align='center')
+        time.sleep(1)
+        t.clear()
 def show0():
+    global digitsEntered
     style = ('Courier', 18, 'bold')
     t1.write('0', font=style, align='center')
     t1.forward(15)
+    digitsEntered.append(0)
 def show1():
+    global digitsEntered
     style = ('Courier', 18, 'bold')
     t1.write('1', font=style, align='center')
     t1.forward(15)
+    digitsEntered.append(1)
 def show2():
+    global digitsEntered
     style = ('Courier', 18, 'bold')
     t1.write('2', font=style, align='center')
     t1.forward(15)
+    digitsEntered.append(2)
 def show3():
+    global digitsEntered
     style = ('Courier', 18, 'bold')
     t1.write('3', font=style, align='center')
     t1.forward(15)
+    digitsEntered.append(3)
 def show4():
+    global digitsEntered
     style = ('Courier', 18, 'bold')
     t1.write('4', font=style, align='center')
     t1.forward(15)
+    digitsEntered.append(4)
 def show5():
+    global digitsEntered
     style = ('Courier', 18, 'bold')
     t1.write('5', font=style, align='center')
     t1.forward(15)
+    digitsEntered.append(5)
 def show6():
+    global digitsEntered
     style = ('Courier', 18, 'bold')
     t1.write('6', font=style, align='center')
     t1.forward(15)
+    digitsEntered.append(6)
 def show7():
+    global digitsEntered
     style = ('Courier', 18, 'bold')
     t1.write('7', font=style, align='center')
     t1.forward(15)
+    digitsEntered.append(7)
 def show8():
+    global digitsEntered
     style = ('Courier', 18, 'bold')
     t1.write('8', font=style, align='center')
     t1.forward(15)
+    digitsEntered.append(8)
 def show9():
+    global digitsEntered
     style = ('Courier', 18, 'bold')
     t1.write('9', font=style, align='center')
     t1.forward(15)
+    digitsEntered.append(9)
 t = Turtle()
 t.penup()
 t.hideturtle()
 def revealanswer():
-    global people, screencolor, t
+    global people, screencolor, t, questionWriter
+    t1.clear()
+    questionWriter.clear()
     gotoEach(people, screencolor, t)
 def cleanup():
+    global digitsEntered
     t1.clear()
     t1.goto(-100, -50)
+    digitsEntered.clear()
 #s.onkey(up, 'Up')
+countdown(timer, 10, time)
 s.onkeyrelease(lambda: cleanup(), 'Delete')
-s.onkeyrelease(lambda: revealanswer(), 'KP_Enter')
 s.onkeyrelease(lambda: show0(), '0')
 s.onkeyrelease(lambda: show1(), '1')
 s.onkeyrelease(lambda: show2(), '2')
@@ -195,10 +260,15 @@ s.onkeyrelease(lambda: show6(), '6')
 s.onkeyrelease(lambda: show7(), '7')
 s.onkeyrelease(lambda: show8(), '8')
 s.onkeyrelease(lambda: show9(), '9')
+s.onkeyrelease(lambda: revealanswer(), 'Return')
 s.listen()
-'''if guess == numcolorpeople:
-    print('You Win')
-else:
-    print('You Lose')'''
-
+print(digitsEntered)
+def listToNum(arr):
+    k = len(arr) - 1
+    answer = 0
+    for i in arr:
+        answer += i * (10** k)
+        k-=1
+    return answer
+print(listToNum(digitsEntered))
 s.exitonclick()
